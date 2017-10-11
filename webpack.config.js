becd6eee246112c0b0e85e24119f83eb3aab1050
Claire Follett <chlaw101@gmail.com>
2017-10-11 09:30:50 -0700

    updating everything

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,20 @@
 import webpack from 'webpack'
 
 var config = {
-  entry: __dirname + '/index.js',
+  entry: __dirname + 'src/index.js',
   output: {
     path: __dirname,
     filename: '/public/bundle.js'
+  },
+  module : {
+    loaders : [
+      {
+        test : '/\.jsx?/',
+        include : __dirname + 'src/index.js',
+        loader : 'babel'
+      }
+    ]
   }
-};
+}
 
 module.exports = config;
